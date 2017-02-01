dec2cb5adf976917cb020183b90c166b4249a3ae
xiaoguang <xiaoguangGzp@gmail.com>
2017-02-01 21:50:09 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/xiaoguang/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.example.xiaoguang.coolweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by xiaoguang on 2017/2/1.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
